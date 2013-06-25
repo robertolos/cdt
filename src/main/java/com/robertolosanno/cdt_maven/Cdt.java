8cf986e1b1b02a0eec20b8f88474b6f3e93fd56d
robertolos <robertolosanno@gmail.com>
2013-06-25 11:51:18 +0200

    mancano solo i vincoli

--- src/main/java/com/robertolosanno/cdt_maven/Cdt.java
+++ src/main/java/com/robertolosanno/cdt_maven/Cdt.java
@@ -10,10 +10,12 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Container;
 import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.GridLayout;
 import java.awt.Paint;
+import java.awt.Polygon;
 import java.awt.Rectangle;
 import java.awt.Shape;
 import java.awt.event.ActionEvent;
@@ -38,7 +40,6 @@ import javax.swing.JToggleButton;
 
 import org.apache.commons.collections15.Factory;
 import org.apache.commons.collections15.Transformer;
-import org.apache.commons.collections15.functors.ChainedTransformer;
 import org.apache.commons.collections15.functors.ConstantTransformer;
 
 import com.hp.hpl.jena.ontology.OntClass;
@@ -50,7 +51,6 @@ import com.hp.hpl.jena.rdf.model.ModelFactory;
 import com.hp.hpl.jena.util.FileManager;
 import com.hp.hpl.jena.util.iterator.ExtendedIterator;
 
-import edu.uci.ics.jung.algorithms.layout.Layout;
 import edu.uci.ics.jung.algorithms.layout.PolarPoint;
 import edu.uci.ics.jung.algorithms.layout.RadialTreeLayout;
 import edu.uci.ics.jung.algorithms.layout.TreeLayout;
@@ -58,7 +58,6 @@ import edu.uci.ics.jung.graph.Forest;
 import edu.uci.ics.jung.graph.DelegateForest;
 import edu.uci.ics.jung.visualization.GraphZoomScrollPane;
 import edu.uci.ics.jung.visualization.Layer;
-import edu.uci.ics.jung.visualization.RenderContext;
 import edu.uci.ics.jung.visualization.VisualizationServer;
 import edu.uci.ics.jung.visualization.VisualizationViewer;
 import edu.uci.ics.jung.visualization.control.CrossoverScalingControl;
@@ -66,11 +65,8 @@ import edu.uci.ics.jung.visualization.control.DefaultModalGraphMouse;
 import edu.uci.ics.jung.visualization.control.ModalGraphMouse;
 import edu.uci.ics.jung.visualization.control.ScalingControl;
 import edu.uci.ics.jung.visualization.decorators.EdgeShape;
-import edu.uci.ics.jung.visualization.decorators.ToStringLabeller;
 import edu.uci.ics.jung.visualization.layout.LayoutTransition;
-import edu.uci.ics.jung.visualization.renderers.BasicVertexLabelRenderer;
 import edu.uci.ics.jung.visualization.renderers.Renderer;
-import edu.uci.ics.jung.visualization.transform.shape.GraphicsDecorator;
 import edu.uci.ics.jung.visualization.util.Animator;
 
 
@@ -82,34 +78,26 @@ public class Cdt extends JApplet {
 	 */
 	
 	public static final String SOURCE_URL = "http://www.semanticweb.org/roberto/ontologies/2013/5/cdt_2";
-
-	protected static final String SOURCE_FILE = "cdt_2c.owl";
-
+	protected static final String SOURCE_FILE = "museum.owl";
 	public static final String NS = SOURCE_URL + "#"; // the namespace of the ontology
 	
     /**
      * the graph
      */
     Forest<String,String> graph;
-
 	Factory<String> edgeFactory = new Factory<String>() {
 		int i=0;
 		public String create() {
 			Integer z=i++;
 			return z.toString();
 		}};
-    
     /**
      * the visual component and renderer for the graph
      */
     VisualizationViewer<String,String> vv;
-    
     VisualizationServer.Paintable rings;
-    
     String root;
-    
     TreeLayout<String,String> treeLayout;
-    
     RadialTreeLayout<String,String> radialLayout;
 
     public Cdt() {
@@ -118,7 +106,6 @@ public class Cdt extends JApplet {
 		loadModel(m);
 		ExtendedIterator<OntClass> iter = m.listHierarchyRootClasses();
 		
-        
         // create a simple graph for the demo
         graph = new DelegateForest<String,String>();
 
@@ -130,80 +117,108 @@ public class Cdt extends JApplet {
         vv =  new VisualizationViewer<String,String>(treeLayout, new Dimension(1200,600));
         vv.setBackground(Color.white);
         vv.getRenderContext().setEdgeShapeTransformer(new EdgeShape.Line());
-        vv.getRenderContext().setVertexLabelTransformer(new ToStringLabeller());
+//        vv.getRenderContext().setVertexLabelTransformer(new ToStringLabeller());
         // add a listener for ToolTips
-        vv.setVertexToolTipTransformer(new ToStringLabeller());
+//        vv.setVertexToolTipTransformer(new ToStringLabeller());
         vv.getRenderContext().setArrowFillPaintTransformer(new ConstantTransformer(Color.lightGray));
+        
         rings = new Rings();
+        
 
-        
-//        
-//        Transformer<String, Paint> vertexPaint = 
-//        	    new Transformer<String, Paint>() {
-//        	        private final Color[] palette = { Color.BLACK, 
-//        	            Color.WHITE, Color.RED };
-//        	 
-//        	        public Paint transform(String i) {
-//        	        	if(i.equals("dim")){
-//        	        		return palette[0];
-//        	        	}
-//        	        	if(i.equals("val")){
-//        	        		return palette[1];
-//        	        	}
-//        	        	return palette[2];
-//        	        }
-//        	    };
-        	    
-//        	    vv.getRenderContext().setVertexLabelTransformer(transformer);
-        	    
-        	    
-//        	    vv.getRenderContext().setVertexLabelTransformer(new MyRenderer());
-        	    
-        
-        
-        
-        
-//        		Transformer labelTransformer = new BasicVertexLabelRenderer<>()
-//
-//        		vv.setVertexLabelTransformer(labelTransformer);
-        
-        
-        
-        
-        
 //        	    Transformer<String, String> transformer = new Transformer<String, String>() {
 //        	        @Override public String transform(String arg0) { return arg0; }
 //        	      };
 //        	      vv.getRenderContext().setEdgeLabelTransformer(transformer);
         	      
         	      
-//        	        vv.getRenderer().setVertexRenderer(new MyRenderer());
-        	        Transformer<String,Paint> vertexColor = new Transformer<String,Paint>() {
-        	            public Paint transform(String s) {
-        	            	String[] ss = s.split("#");
-        	            	String type= ss[1];
-        	                if(type.equals("root")) return Color.BLUE;
-        	                if(type.equals("dim")) return Color.BLACK;
-        	                if(type.equals("val")) return Color.WHITE;
-        	                if(type.equals("par")) return Color.GRAY;
-        	                
-        	                return Color.GREEN;
-        	            }
-        	        };
-        	        vv.getRenderContext().setVertexFillPaintTransformer(vertexColor);
         	        
-        	        Transformer<String, String> transformer = new Transformer<String, String>() {
-          	          @Override public String transform(String arg0) {
-          	        	  String[] node = arg0.split("#");
-          	        	  return node[0]; 
-          	        }
-          	        };
-          	        vv.getRenderContext().setVertexLabelTransformer(transformer);
-//          	        transformer = new Transformer<String, String>() {
-//          	          @Override public String transform(String arg0) { return arg0; }
-//          	        };
-//          	        vv.getRenderContext().setEdgeLabelTransformer(transformer);
+        
+		//***************** MODIFICA COLORE VERTICE ************************
+        Transformer<String,Paint> vertexColor = new Transformer<String,Paint>() {
+            public Paint transform(String s) {
+            	String[] ss = s.split("#");
+            	String type= ss[1];
+                if(type.equals("root")) return Color.lightGray;
+                if(type.equals("dim")) return Color.BLACK;
+                if(type.equals("val")) return Color.WHITE;
+                if(type.equals("par_val")) return Color.WHITE;
+                if(type.equals("par_dim")) return Color.WHITE;
+                
+                return Color.GREEN;
+            }
+        };
+        vv.getRenderContext().setVertexFillPaintTransformer(vertexColor);
+        
+        //**************** MODIFICA FORMA VERTICE ***************************
+        
+        //Dati per creare un triangolo (lo creaiamo attraverso la classe Polygon)
+        final int[]xShape = new int[4];
+        final int[]yShape = new int[4];
+        final int nShape;  // count of points
+        // Make a shape
+        xShape[0]=-10; xShape[1]=0; xShape[2]=10;
+        yShape[0]=0; yShape[1]=20; yShape[2]=0;
+        nShape = 3;
+        
+        Transformer<String, Shape> vertexShape = new Transformer<String, Shape>() {
+        	        private final Shape[] styles = {
+        	            new Rectangle(-10, -10, 20, 20),
+        	            new Ellipse2D.Double(-10, -10, 20, 20),
+        	            new Polygon(xShape, yShape, nShape) //Triangolo
+        	            
+        	            };
         	        
+        	 
+        	        @Override
+        	        public Shape transform(String i) {
+        	        	String[] type = i.split("#");
+        	        	if(type[1].equals("par_val")){
+        	        		return styles[0];
+        	        	}else if(type[1].equals("par_dim")){
+        	        		return styles[2];
+        	        	}else{
+        	        		return styles[1];
+        	        	}
+        	        }
+        	    };  
+        vv.getRenderContext().setVertexShapeTransformer(vertexShape);
+	    
+//        	        	    vv.getRenderer().setVertexRenderer(new MyRenderer());
+        
+        
+        
+      //**************** MODIFICA FONT LABEL ***************************
+        vv.getRenderContext().setVertexFontTransformer(new Transformer<String, Font>(){
+
+			@Override
+			public Font transform(String arg0) {
+				Font font = new Font("Arial Unicode MS", Font.PLAIN, 11);
+				return font;
+			}});
+        
+
+        
+     
+
+        
+        
+        // ********************** POSIZIONA LA LABEL SOTTO IL VERTICE ****************************
+        vv.getRenderer().getVertexLabelRenderer().setPosition(Renderer.VertexLabel.Position.CNTR);
+        vv.getRenderer().getVertexLabelRenderer().setPosition(Renderer.VertexLabel.Position.S);
+        	    
+        
+	    //******************** RIMUOVE DAL TESTO DELLA LABEL DEL VERTICE IL TIPO DI VERTICE ************************
+        
+        Transformer<String, String> transformer = new Transformer<String, String>() {
+          @Override public String transform(String arg0) {
+        	  String[] node = arg0.split("#");
+        	  return node[0]; 
+        }
+        };
+        
+        vv.getRenderContext().setVertexLabelTransformer(transformer);
+        
+          	        
           	      
         Container content = getContentPane();
         final GraphZoomScrollPane panel = new GraphZoomScrollPane(vv);
@@ -267,31 +282,38 @@ public class Cdt extends JApplet {
 
         content.add(controls, BorderLayout.SOUTH);
     }
-    static class MyRenderer implements Renderer.Vertex<String, String> {
-        @Override public void paintVertex(RenderContext<String, String> rc, Layout<String, String> layout, String vertex) {
-          GraphicsDecorator graphicsContext = rc.getGraphicsContext();
-          Point2D center = layout.transform(vertex);
-          Shape shape = null;
-          Color color = null;
-//          System.out.println("Vert: "+vertex);
-          if(vertex.equals("root")) {
-//            shape = new Rectangle((int)center.getX()-10, (int)center.getY()-10, 20, 20);
-            color = new Color(127, 127, 0);
-          } else if(vertex.equals("dim")) {
-//            shape = new Rectangle((int)center.getX()-10, (int)center.getY()-20, 20, 40);
-            color = new Color(127, 0, 127);
-          } else if(vertex.equals("val")) {
+//    static class MyRenderer implements Renderer.Vertex<String, String> {
+//        @Override public void paintVertex(RenderContext<String, String> rc, Layout<String, String> layout, String vertex) {
+//          GraphicsDecorator graphicsContext = rc.getGraphicsContext();
+//          Point2D center = layout.transform(vertex);
+//          String[] temp = vertex.split("#");
+//          String type=temp[1];
+//          Shape shape = null;
+//          Color color = null;
+//          System.out.println("t: "+type);
+//          if(type.equals("root")) {
 //            shape = new Ellipse2D.Double(center.getX()-10, center.getY()-10, 20, 20);
-            color = new Color(0, 127, 127);
-          }else{
-        	  color = new Color(0, 127, 0);
-        	  shape = new Rectangle((int)center.getX()-10, (int)center.getY()-10, 20, 20);
-          }
-          graphicsContext.setPaint(color);
+//            color = new Color(255, 0, 0);
+//          } else if(type.equals("dim")) {
+//            shape = new Ellipse2D.Double(center.getX()-10, center.getY()-10, 20, 20);
+//            color = new Color(0, 0, 0);
+//          } else if(type.equals("val")) {
+//            shape = new Ellipse2D.Double(center.getX()-10, center.getY()-10, 20, 20);
+//            color = new Color(0, 255, 255);
+//          }else if(type.equals("par")) {
+//            shape = new Rectangle((int)center.getX()-10, (int)center.getY()-10, 20, 20);
+//            color = new Color(0, 255, 255);
+//          }
+//          else{
+//        	  color = new Color(0, 127, 0);
+//        	  shape = new Rectangle((int)center.getX()-10, (int)center.getY()-10, 20, 20);
+//          }
+//          graphicsContext.setPaint(color);
 //          graphicsContext.fill(shape);
-        }
-      }
+//        }
+//      }
     
+    // CARICA L'ONTOLOGIA DAL FILE
     protected void loadModel(OntModel m) {
 		FileManager.get().getLocationMapper().addAltEntry(SOURCE_URL, SOURCE_FILE);
 		Model baseOntology = FileManager.get().loadModel(SOURCE_URL);
@@ -299,6 +321,8 @@ public class Cdt extends JApplet {
 		m.setNsPrefix("st", NS);
 	}
     
+    
+    // PER LA VISUALIZZAZIONE RADIALE
     class Rings implements VisualizationServer.Paintable {
     	
     	Collection<Double> depths;
@@ -345,6 +369,15 @@ public class Cdt extends JApplet {
 			OntClass rootClass = iter.next();
 //			System.out.println("Root Class: " + rootClass.toString());
 			getSubClasses("root#root",rootClass);
+			
+			//Verifico se le dimensioni figle di root hanno parametri
+			ExtendedIterator<? extends OntResource> inst= rootClass.listInstances();
+			while (inst.hasNext()) {
+				OntResource instance = inst.next();
+				System.out.println("Dim: "+rootClass+"; INstance: " + instance.getURI());
+				plot(rootClass.getURI(), "dim", instance.getURI(), "par_dim");
+			} 
+			
 		} 
     }
 
@@ -354,10 +387,11 @@ public class Cdt extends JApplet {
 		
 		String[] padre_splitted = padre.split("#");
 		String radice = padre_splitted[0];
-//		System.out.println("padre:"+radice);
 		if (radice.equals("root")) {
 			plot(padre, "root", rootClass.toString(), "dim");
 		}
+		
+		
 		for (Iterator<OntClass> i = rootClass.listSubClasses(); i.hasNext();) {
 			OntClass c = i.next();
 			
@@ -367,9 +401,6 @@ public class Cdt extends JApplet {
 			}else{
 				type_padre="dim";
 			}
-			
-			
-			
 			
 			String comment_node = c.getComment(null);
 			if(comment_node!=null && comment_node.equals("value")){
@@ -381,19 +412,27 @@ public class Cdt extends JApplet {
 			String papa=rootClass.getURI();
 			String node=c.getURI();
 			String nodebak=node;
+			String typenodebak=type_node;
 			ExtendedIterator<? extends OntResource> inst= c.listInstances();
-			int j=0;
+			
+			plot(papa, type_padre, node, type_node);
+			
+			//Verifica esistenza parametri figli (i parametri sono modellati in Protégé tramite Istanze)
 			while (inst.hasNext()) {
 				OntResource instance = inst.next();
-//				type_nome="par"
-				System.out.println("Node: "+node+"; INstance: " + instance.getURI());
+//				System.out.println("Node: "+node+"; INstance: " + instance.getURI());
 				papa=nodebak;
+				type_padre=typenodebak;
 				node=instance.getURI();
+				if(type_padre.equals("dim")){
+					type_node="par_dim";
+				}else{
+					type_node="par_val";
+				}
+				
 				plot(papa, type_padre, node, type_node);
-				j++;
 			} 
-			if(j==0)
-				plot(papa, type_padre, node, type_node);
+			
 			getSubClasses(c.getURI(), c);
 		}
 	}
@@ -408,7 +447,7 @@ public class Cdt extends JApplet {
      * a driver for this demo
      */
     public static void main(String[] args) {
-        JFrame frame = new JFrame();
+        JFrame frame = new JFrame("CDT Viewer");
         Container content = frame.getContentPane();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
